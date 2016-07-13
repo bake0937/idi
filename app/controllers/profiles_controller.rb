572cdd51d39f6865f2d9159530088f6cb4f7eb5a
bake0937 <sadayasu.info@gmail.com>
2016-07-13 12:33:24 +0900

    [update] update controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,23 @@
 class ProfilesController < ApplicationController
+
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    profile = Profile.find(params[:id])
+    profile.update(update_params)
+    flash[:success] = 'プロフィール情報を変更しました'
+    redirect_to :action => "edit"
+  end
+
+  private
+  def update_params
+    params.require(:profile).permit(:nickname, :text)
+  end
+
 end
