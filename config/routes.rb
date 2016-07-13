773fd663bdec8168d0c6bdf4e07cb323b0d3f116
bake0937 <sadayasu.info@gmail.com>
2016-07-13 12:33:04 +0900

    [update] update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => 'users/registrations'}
   root to: "home#index"
+  resources :profiles,only: [:show, :edit, :update]
 end
