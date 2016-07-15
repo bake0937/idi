6dc87581dd59f821a7c4e44277a3a8e796481e9c
bake0937 <sadayasu.info@gmail.com>
2016-07-15 10:28:45 +0900

    [add] add articles/edit to /views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,9 +13,26 @@ class ArticlesController < ApplicationController
     redirect_to controller: :home, action: :index
   end
 
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update(update_params)
+    flash[:success] = '更新しました'
+    redirect_to controller: :home, action: :index
+  end
+
   private
   def create_params
     params.require(:article).permit(:title, :category, :detail, :cue, :feature, :point, :remark ).merge(user_id: current_user.id)
   end
+
+  def update_params
+    params.require(:article).permit(:title, :category, :detail, :cue, :feature, :point, :remark ).merge(user_id: current_user.id)
+  end
+
 
 end
