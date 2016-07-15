4ca587fdda06e3607ed82dbddb82c546f8171bfc
bake0937 <sadayasu.info@gmail.com>
2016-07-15 10:28:14 +0900

    [add] add articles/new to /views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(create_params)
+    redirect_to controller: :home, action: :index
+  end
+
+  private
+  def create_params
+    params.require(:article).permit(:title, :category, :detail, :cue, :feature, :point, :remark ).merge(user_id: current_user.id)
+  end
+
 end
