aac01319aa61607a0fa471a8829fe1df269588f9
bake0937 <sadayasu.info@gmail.com>
2016-07-17 18:20:33 +0900

    [update] update reviews_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.create(create_params)
+    redirect_to article_path(params[:article_id])
+  end
+
+  private
+  def create_params
+    params.require(:review).permit(:comment).merge(user_id: current_user.id, article_id: params[:article_id])
+  end
 end
