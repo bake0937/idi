5499904e3c2f5ba4c92015053e09ee9446e7c4bf
bake0937 <sadayasu.info@gmail.com>
2016-07-17 18:26:03 +0900

    [update] update profile_model
    need to nickname_to_username

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,12 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  #nicknameがnilの場合、usernameの値を格納
+  def nickname_to_username
+    if nickname.blank?
+      "@" + user.username
+    else
+      nickname
+    end
+  end
 end
