55046a59c25c47f618d41d9e39f52d3f4cc6d363
bake0937 <sadayasu.info@gmail.com>
2016-07-25 18:14:42 +0900

    [add] add destroy to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => 'users/registrations'}
   root to: "home#index"
   resources :profiles, only: [:show, :edit, :update]
-  resources :articles, only: [:show, :new, :create, :edit, :update] do
-    resources :reviews, only: [:create]
+  resources :articles, only: [:show, :new, :create, :edit, :update, :destroy] do
+    resources :reviews, only: [:create, :destroy]
   end
 end
