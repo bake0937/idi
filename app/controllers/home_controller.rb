64249148cce8a8a864483d7fded850de83110829
bake0937 <sadayasu.info@gmail.com>
2016-08-05 12:30:24 +0900

    [add] add slide to home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @articles = Article.joins(:user).order("articles . updated_at DESC").page(params[:page]).per(8)
+    @slides = Article.joins(:user).order('RAND()').limit(5)
   end
 
 end
