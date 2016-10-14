e40241b93e54ef152598f8753204a7a517da60c0
bake0937 <sadayasu.info@gmail.com>
2016-10-14 21:46:18 +0900

    [change] change it to carry out current_article earlier

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
-  before_action :correct_user, only: [:edit, :update]
   before_action :current_article, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @review = Review.new
