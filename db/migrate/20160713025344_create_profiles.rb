5311ca3f88ded15b0fbe9dda590584de2e46a349
bake0937 <sadayasu.info@gmail.com>
2018-10-01 02:13:17 +0900

    rails4 で利用したmigrationファイルを rails5 へ移行
    find db/migrate -name '*.rb' | xargs sed -i  's/ActiveRecord::Migration/ActiveRecord::Migration[4.2]/g'

--- db/migrate/20160713025344_create_profiles.rb
+++ db/migrate/20160713025344_create_profiles.rb
@@ -1,9 +1,9 @@
-class CreateProfiles < ActiveRecord::Migration
+class CreateProfiles < ActiveRecord::Migration[4.2]
   def change
     create_table :profiles do |t|
       t.string :nickname
       t.string :introduction
-      t.references :user, index: true, foreign_key: true, type: :integer
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
