0c86a922f58e20bb3b3fb2957ce99e4b4921c3f1
bake0937 <sadayasu.info@gmail.com>
2018-10-01 02:21:12 +0900

    rspec動作確認用にspecを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    username "test9999"
+    email "tester@example.com"
+    password "testtest9999"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid without an email address" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("を入力してください。")
+  end
 end
